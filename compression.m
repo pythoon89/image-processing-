5527a654ad2ec356c04749d46502768b1d5a0807
pythoon89 <34901147+pythoon89@users.noreply.github.com>
2018-01-03 23:00:59 +0000

    compression threshold

---
+++ compression.m
@@ -0,0 +1,26 @@
+barbara=imread('C:\Users\Admin\Desktop\DA\Image and Video Analysisi\coursework\data\barbara.bmp');
+barbara=im2double(barbara);
+subplot(1,2,1);
+imshow(barbara)
+
+bar1=blkproc(barbara,[8 8],'dctforward(x)');
+bar1=comthresholding(bar1,0.1);
+bar=blkproc(bar1,[8 8],'dctbackward(x)');
+subplot(1,2,2);
+imshow(bar);
+
+function D=comthresholding(I,s)
+sz=size(I);
+newI=zeros(sz(1),sz(1));
+for i=1:sz
+    for j=1:sz
+        if I(i,j)>=s || I(i,j)<=0-s
+            newI(i,j)=I(i,j);
+        else
+            newI(i,j)=0;
+        end
+    end
+end
+D=newI;
+end
+           
